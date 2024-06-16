92f26aaecda2c77536f93c26ef9e5e87e23c80ac
DavidoTek <54072917+DavidoTek@users.noreply.github.com>
2024-06-16 22:43:45 +0200

    asb_node: Add class representation for IO Modules

--- tools/asysbuslib/asb_node.py
+++ tools/asysbuslib/asb_node.py
@@ -1,4 +1,18 @@
 from dataclasses import dataclass
+from enum import Enum
+
+
+class AsbIoModuleType(Enum):
+    """ ASB I/O module types """
+    ASB_IO_DIN = 1
+    ASB_IO_DOUT = 2
+
+
+@dataclass
+class AsbIoModule:
+    """ Data class for representing an ASB I/O module """
+    cfg_id: int
+    mod_type: AsbIoModuleType
 
 
 @dataclass
@@ -9,3 +23,5 @@ class AsbNode:
     boot_time: int
     last_seen: int
     reported_uptime_days: int
+
+    io_modules: list[AsbIoModule]
