3dd7d7dbea74c76d4108b45c0dcca2099a6a4176
DavidoTek <54072917+DavidoTek@users.noreply.github.com>
2024-06-17 16:25:02 +0200

    asb_node: Add Docstrings

--- tools/asysbuslib/asb_node.py
+++ tools/asysbuslib/asb_node.py
@@ -10,14 +10,29 @@ class AsbIoModuleType(Enum):
 
 @dataclass
 class AsbIoModule:
-    """ Data class for representing an ASB I/O module """
+    """
+    Data class for representing an ASB I/O module
+    
+    Attributes:
+        cfg_id (int): Config ID of the ASB I/O module
+        mod_type (AsbIoModuleType): Module type if known
+    """
     cfg_id: int
     mod_type: AsbIoModuleType
 
 
 @dataclass
 class AsbNode:
-    """ Data class for representing an ASB node """
+    """
+    Data class for representing an ASB node
+    
+    Attributes:
+        id (int): Node ID
+        boot_time (int): UNIX timestamp of the boot message or -1
+        last_seen (int): UNIX timestamp of the last message received from the node or -1
+        reported_uptime_days (int): Uptime in days reported by the node or -1
+        io_modules (list[AsbIoModule]): List of ASB I/O modules of the node
+    """
     id: int
 
     boot_time: int
